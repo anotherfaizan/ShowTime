c0ac5fc09d7f067032f3015fb76ad968dbb360ad
Shaik Abdul Irfan <abdulirfan0225@gmail.com>
2025-04-21 13:57:50 +0530

    initial commit

---
+++ src/main/java/com/ShowTime/ShowTimeApp/ShowTimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.ShowTime.ShowTimeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowTimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowTimeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ShowTime/ShowTimeApp/ShowTimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShowTime.ShowTimeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowTimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
